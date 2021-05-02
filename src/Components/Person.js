6d23179d76456d0abb7a4bd4c1ba1415938f1234
Navin Kumar M <navin.vamosys@gmail.com>
2021-05-02 18:28:39 +0530

    formatted

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export const Person = (props) => {
-    return (
-        <div>
-<p>            person name is  {props.name}  and is age is {props.age}</p>
-<p>{props.children     }</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>
+        {" "}
+        person name is {props.name} and is age is {props.age}
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
